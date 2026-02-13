042bd68d1dab3106418b1927241de6392df35806
Fabian Schwab <fabian.schwab@ibm.com>
2026-02-13 09:09:19 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Button } from 'carbon-components-svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen w-full items-center justify-center">
+	<Button href="/demo">Demo Page</Button>
+</div>
