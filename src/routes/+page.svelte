7c366fd90578bf5193d95f3a186c9b29b4d1731e
Fabian Schwab <fabian.schwab@ibm.com>
2026-02-13 12:51:52 +0100

    css order and all tailwind styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,16 @@
 	import { Button } from 'carbon-components-svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="flex h-screen w-full items-center justify-center">
+<div class="flex h-screen w-full flex-col items-center justify-center">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<h1>Carbon design</h1>
+	<p class="mb-4">
+		Visit <a href="https://svelte.carbondesignsystem.com/">Carbon Svelte Components</a> to read the
+		component documentation. <br />
+		Where in the upper right corner you can find links to Icons, Charts and Pictograms.
+	</p>
 	<Button href="/demo">Demo Page</Button>
 </div>
