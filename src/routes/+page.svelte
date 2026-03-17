2d04b6f6904e27d90483d3a95ae62cf08ca9ab44
Fabian Schwab <fabian.schwab@ibm.com>
2026-03-17 12:42:25 +0100

    example overview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Welcome from '$lib/components/Welcome.svelte';
+	import { route } from '$lib/ROUTES';
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center">
 	<Welcome />
+	<p class="mt-8">See some examples <a href={route('/examples')}>here</a></p>
 </div>
---
+++ src/routes/examples/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { routes } from '$lib/ROUTES';
+</script>
+
+<h1>Examples</h1>
+<p>Gets dyamically updated as new routes are made.</p>
+<ul>
+	{#each routes as route}
+		{#if route.startsWith('/examples/')}
+			<li><a href={route}>{route}</a></li>
+		{/if}
+	{/each}
+</ul>
